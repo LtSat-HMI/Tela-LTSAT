c6d9e723176cf9cdceeaf7fc45a54f5fdc173efd
gui <guigon@alunos.utfpr.edu.br>
2023-08-21 00:27:47 -0300

    feat: reading altitude from txt file

--- src/ flight-report.py
+++ src/ flight-report.py
@@ -3,15 +3,31 @@ import numpy as np
 import pandas as pd
 
 dataFrame = {
-            'altitude': [30, 40, 50 ,60],
+            'altitude': [0, 40, 50 ,60],
+            'temperature': [30, 40, 50 ,60],
+            'battery-voltage': [30, 40, 50 ,60],
+            'air-pressure': [30, 40, 50 ,60],
+            'gps-altitude': [30, 40, 50 ,60],
+            'gps-latitude': [30, 40, 50 ,60],
+            'gps-longitude': [30, 40, 50 ,60],
+            'humidity': [30, 40, 50 ,60],
             'time': [1, 2, 3 ,4]
         }
 
-df = pd.DataFrame(dataFrame)
+df = pd.DataFrame(dataFrame, columns=['a', 'b', 'c', 'd'])
 
-print("Line graph: ")
-plt.plot(df["time"], df["altitude"])
-plt.show()
+f = open('Dados_1h24.txt')
+
+for line in f:
+    if '| A:' in line:
+        altitude = line.split(' ')[2].strip().replace('A:','')
+        print(altitude)
+
+
+
+# print("Line graph: ")
+# plt.plot(df["time"], df["altitude"])
+# plt.show()
 
 
 
